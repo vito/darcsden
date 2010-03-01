fc9e88b7d704af70040dc563d06a14ffcf265703
Alex Suraci <i.am@toogeneric.com>
2010-03-01 08:45:17 +0000

    Cleaned up DarcsDen.Handler a little bit.

--- DarcsDen/Handler.hs
+++ DarcsDen/Handler.hs
@@ -6,12 +6,7 @@ import Text.JSON.Generic
 import Data.ByteString.Lazy.Char8 (unpack, split, pack)
 
 
-doPage :: String -> [JSValue] -> Application
-doPage page context env = renderToResponse env ("html/" ++ page ++ ".html") context
-
-var :: Data a => String -> a -> JSValue
-var key val = JSObject $ toJSObject [(key, toJSON val)]
-
+-- Pages
 index :: Application
 index = doPage "index" []
 
@@ -21,12 +16,22 @@ user name = doPage "user" [var "name" name]
 notFound :: Application
 notFound = doPage "404" []
 
-appFor :: [String] -> Application
-appFor [] = index
-appFor ["index"] = index
-appFor ["user", name] = user name
-appFor _ = notFound
 
+-- Page helpers
+doPage :: String -> [JSValue] -> Application
+doPage page context env = renderToResponse env ("html/" ++ page ++ ".html") context
+
+var :: Data a => String -> a -> JSValue
+var key val = JSObject $ toJSObject [(key, toJSON val)]
+
+
+-- URL handling
 handler :: Application
-handler env = appFor path env
-              where path = map unpack (split '/' (pack (tail (pathInfo env))))
+handler env = pageFor path env
+    where path = map unpack (split '/' (pack (tail (pathInfo env))))
+
+pageFor :: [String] -> Application
+pageFor [] = index
+pageFor ["index"] = index
+pageFor ["user", name] = user name
+pageFor _ = notFound
