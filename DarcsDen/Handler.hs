98c843ffe1e0355a6396ff6306568c6a4429a848
Alex Suraci <i.am@toogeneric.com>
2010-03-05 21:05:58 +0000

    Added basic logging-in (not actually functioning yet; no sessions), and cleaned up some debugging junk.

--- DarcsDen/Handler.hs
+++ DarcsDen/Handler.hs
@@ -7,7 +7,7 @@ import System.Time (getClockTime)
 import Text.JSON.Generic
 import Data.ByteString.Lazy.Char8 (unpack, split, pack)
 import Data.Char (isAlphaNum)
-import Data.Map (toList, (!))
+import Data.Map ((!))
 
 import DarcsDen.HackUtils
 import DarcsDen.State.User
@@ -19,42 +19,59 @@ index :: Application
 index = doPage "index" []
 
 user :: String -> Application
-user name env = do user <- query $ GetUser name
-                   print env
-                   doPage "user" [var "user" user, var "test" (getInput "test" env), var "another" (getInput "another" env)] env
+user name env = do u <- query $ GetUser name
+                   doPage "user" [var "user" u] env
 
 register :: Application
-register env = case requestMethod env of
-                 GET -> doPage "register" [] env
-                 POST ->
-                     validate env [ when
-                                      (nonEmpty "name")
-                                      (\(OK r) -> io (do check <- query (GetUser (r ! "name"))
-                                                         return (check == Nothing))
-                                                     "name is already in use")
-                                  , predicate "name" (and . map isAlphaNum) "be alphanumeric"
-                                  , nonEmpty "email"
-                                  , when (nonEmpty "password1" `And` nonEmpty "password2") (const $ equal "password1" "password2")
-                                  , predicate "email" (const True) "be a valid email"
-                                  ]
-                     (\(OK r) -> do
-                        now <- getClockTime
-                        salt <- salt 32
-                        update $ AddUser (User { uName = r ! "name"
-                                               , uPassword = hashPassword (r ! "password1") salt
-                                               , uSalt = salt
-                                               , uFullName = ""
-                                               , uWebsite = ""
-                                               , uEmail = r ! "email"
-                                               , uPubkeys = []
-                                               , uJoined = now
-                                               })
-                        user <- query $ GetUser (r ! "name")
-                        doPage "register" [var "passed" (toList r), var "user" user] env)
-                     (\(Invalid failed) ->
-                        doPage "register" [ var "failed" (map explain failed)
-                                          , assocVar "in" (getInputs env)
-                                          ] env)
+register env@(Env { requestMethod = GET }) = doPage "register" [] env
+register env = validate env [ when (nonEmpty "name")
+                                   (\(OK r) -> io "name is already in use" $ do
+                                                 u <- query (GetUser (r ! "name"))
+                                                 return (u == Nothing))
+                            , predicate "name" (and . map isAlphaNum) "be alphanumeric"
+                            , nonEmpty "email"
+                            , when (nonEmpty "password1" `And` nonEmpty "password2")
+                                   (const $ equal "password1" "password2")
+                            , predicate "email" (const True) "be a valid email"
+                            ]
+               (\(OK r) -> do
+                  now <- getClockTime
+                  s <- salt 32
+                  update $ AddUser (User { uName = r ! "name"
+                                         , uPassword = hashPassword (r ! "password1") s
+                                         , uSalt = s
+                                         , uFullName = ""
+                                         , uWebsite = ""
+                                         , uEmail = r ! "email"
+                                         , uPubkeys = []
+                                         , uJoined = now
+                                         })
+                  doPage "register" [var "success" True] env)
+               (\(Invalid failed) ->
+                    doPage "register" [ var "failed" (map explain failed)
+                                      , assocObj "in" (getInputs env)
+                                      ] env)
+
+login :: Application
+login env@(Env { requestMethod = GET }) = doPage "login" [] env
+login env = validate env
+            [ when
+                (nonEmpty "name" `And` nonEmpty "password")
+                (\(OK r) ->
+                     io "invalid username or password" $ do
+                       c <- query $ GetUser (r ! "name")
+                       case c of
+                         Nothing -> return False
+                         Just u -> let hashed = hashPassword (r ! "password") (uSalt u)
+                                   in return $ uPassword u == hashed)
+            ]
+            (\(OK r) -> do
+               setCookies [("user", r ! "name")] $
+                          doPage "index" [] env)
+            (\(Invalid failed) ->
+                 doPage "login" [ var "failed" (map explain failed)
+                                , assocObj "in" (getInputs env)
+                                ] env)
 
 notFound :: Application
 notFound = doPage "404" []
@@ -67,8 +84,8 @@ doPage page context env = renderToResponse env ("html/" ++ page ++ ".html") cont
 var :: Data a => String -> a -> JSValue
 var key val = JSObject $ toJSObject [(key, toJSON val)]
 
-assocVar :: Data a => String -> [(String, a)] -> JSValue
-assocVar key val = JSObject $ toJSObject [(key, JSObject . toJSObject . map (\(k, v) -> (k, toJSON v)) $ val)]
+assocObj :: Data a => String -> [(String, a)] -> JSValue
+assocObj key val = JSObject $ toJSObject [(key, JSObject . toJSObject . map (\(k, v) -> (k, toJSON v)) $ val)]
 
 -- URL handling
 handler :: Application
@@ -80,5 +97,6 @@ pageFor [] = index
 pageFor ["index"] = index
 pageFor ["user", name] = user name
 pageFor ["register"] = register
+pageFor ["login"] = login
 pageFor _ = notFound
 
