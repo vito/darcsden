9116b66fac7e340a99949b0630730fc7522991a7
Alex Suraci <i.am@toogeneric.com>
2010-03-08 03:45:47 +0000

    Make sure requested paths to public/ are sane.

--- DarcsDen/Handler.hs
+++ DarcsDen/Handler.hs
@@ -3,9 +3,9 @@ module DarcsDen.Handler where
 import Hack
 import Hack.Contrib.Mime
 import Happstack.State
-import System.Directory (doesFileExist)
+import System.Directory (doesFileExist, canonicalizePath, makeRelativeToCurrentDirectory)
 import Data.ByteString.Lazy.Char8 (unpack, split, pack)
-import Data.List (intercalate)
+import Data.List (intercalate, isPrefixOf)
 import System.FilePath (takeExtension)
 import qualified Data.ByteString.Lazy as LS
 
@@ -35,12 +35,19 @@ pageFor ["register"] = register
 pageFor ["login"] = login
 pageFor ["logout"] = logout
 pageFor ["init"] = initialize
-pageFor ("public":p) = \s e -> do exists <- doesFileExist ("public/" ++ intercalate "/" p)
-                                  if exists
-                                    then do file <- LS.readFile ("public/" ++ intercalate "/" p)
-                                            let mime = maybe "text/plain" id $ lookup_mime_type (takeExtension (last p))
-                                            return (Response 200 [("Content-Type", mime)] file)
-                                    else notFound s e
+pageFor ("public":unsafe) = \s e ->
+  do safe <- canonicalizePath ("public/" ++ intercalate "/" unsafe) >>= makeRelativeToCurrentDirectory
+     exists <- doesFileExist safe
+
+     -- Make sure there's no trickery going on here.
+     if not ("public/" `isPrefixOf` safe && exists)
+        then notFound s e
+        else do
+
+     let mime = maybe "text/plain" id $ lookup_mime_type (takeExtension safe)
+
+     file <- LS.readFile safe
+     return (Response 200 [("Content-Type", mime)] file)
 pageFor [name] = user name
 pageFor [name, repo] = repository name repo
 pageFor (name:repo:"browse":file) = browseRepository name repo file
