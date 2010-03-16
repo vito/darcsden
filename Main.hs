9301651e7caade15ce195a2ffb5a6f2935160ca6
Alex Suraci <i.am@toogeneric.com>
2010-03-16 18:45:40 +0000

    Ensure that darcsden is run as root.

--- Main.hs
+++ Main.hs
@@ -4,6 +4,7 @@ import Control.Concurrent (forkIO)
 import Hack.Handler.Happstack (run)
 import Happstack.State
 import System.Environment (withProgName)
+import System.Posix.User (getRealUserID)
 
 import DarcsDen.Handler
 import DarcsDen.State
@@ -11,6 +12,12 @@ import DarcsDen.State
 
 main :: IO ()
 main = withProgName "darcsden" $ do
+         user <- getRealUserID
+
+         if user /= 0
+           then putStrLn "darcsden must be run as root."
+           else do
+
          state <- startSystemState (Proxy :: Proxy State)
          forkIO (run handler)
          waitForTermination
