539affc84bc4262a6dbd01881c0c04cfb83f15a5
Alex Suraci <i.am@toogeneric.com>
2010-03-19 21:40:20 +0000

    Fixed up admin shell stuff, and added unset command.

--- Main.hs
+++ Main.hs
@@ -43,10 +43,22 @@ main = withProgName "darcsden" $ do
          shutdownSystem state
 
 runCommand :: [String] -> InputT IO ()
-runCommand ["help"] = outputStrLn (unlines [ "exit: exit"
-                                           , "help: show this dialogue"
-                                           , "get (type) (key): get a (type) in the database by key (key)"
-                                           ])
+runCommand ["help"] = cols [ ("exit", "shut down darcsden")
+                           , ("help", "show this message")
+                           , ("get user (name)", "print user (name)")
+                           , ("get repo (owner) (name)", "print (owner)'s (name) repository")
+                           , ("set user (name) name (newname)", "rename a user to (newname)")
+                           , ("set user (name) fullname (value)", "set a user's fullname to (value)")
+                           , ("set user (name) website (value)", "set a user's website to (value)")
+                           , ("set user (nwame) email (value)", "set a user's email to (value)")
+                           , ("set repo (owner) (name) name (newname)", "rename a repository to (newname)")
+                           , ("set repo (owner) (name) owner (newowner)", "change a repository's owner to (newowner)")
+                           , ("set repo (owner) (name) description (value)", "set a repository's description to (value)")
+                           , ("set repo (owner) (name) website (value)", "set a repository's website to (value)")
+                           , ("set repo (owner) (name) fork (fork owner) (fork name)", "set the repository that this repository is a fork of")
+                           , ("unset user (name) (what)", "unset user (name)'s (what)")
+                           , ("unset repo (owner) (name) (what)", "unset (owner)'s (name) repository's (what)")
+                           ]
 runCommand ["get", "user", name]
   = do user <- query (GetUser name)
        case user of
@@ -91,33 +103,49 @@ runCommand ["destroy", "repo", user, name]
          Just "y" -> do lift (dirty (destroyRepository (user, name)))
                         outputStrLn "Repository destroyed."
          _ -> return ()
-runCommand ["set", "user", name, what, value]
+runCommand ("set":"user":name:what:value)
   = do user <- query (GetUser name)
        case user of
          Nothing -> outputStrLn "User not found."
          Just u ->
            case what of
-             "name" -> lift (dirty (renameUser value u)) >> return ()
-             "fullname" -> update (UpdateUser (u { uFullName = value }))
-             "website" -> update (UpdateUser (u { uWebsite = value }))
-             "email" -> update (UpdateUser (u { uEmail = value }))
+             "name" -> lift (dirty (renameUser (head value) u)) >> return ()
+             "fullname" -> update (UpdateUser (u { uFullName = unwords value }))
+             "website" -> update (UpdateUser (u { uWebsite = head value }))
+             "email" -> update (UpdateUser (u { uEmail = head value }))
              _ -> outputStrLn ("Don't know how to update user's " ++ what ++ ".")
-runCommand ["set", "repo", user, name, what, value]
+runCommand ("set":"repo":user:name:what:value)
   = do repo <- query (GetRepository (user, name))
        case repo of
          Nothing -> outputStrLn "Repository not found."
          Just r ->
            case what of
-             "name" -> lift (dirty (renameRepository value r)) >> return ()
-             "owner" -> lift (dirty (changeRepositoryOwner value r)) >> return ()
-             "descrpition" -> update (UpdateRepository (r { rDescription = value }))
-             "website" -> update (UpdateRepository (r { rWebsite = value }))
-             "fork" ->
-               let fork = if value == "_"
-                            then Nothing
-                            else Just (read value)
-               in update (UpdateRepository (r { rForkOf = fork }))
-             _ -> outputStrLn ("Don't know how to update user's " ++ what ++ ".")
+             "name" -> lift (dirty (renameRepository (head value) r)) >> return ()
+             "owner" -> lift (dirty (changeRepositoryOwner (head value) r)) >> return ()
+             "description" -> update (UpdateRepository (r { rDescription = unwords value }))
+             "website" -> update (UpdateRepository (r { rWebsite = head value }))
+             "fork" -> update (UpdateRepository (r { rForkOf = Just (value !! 0, value !! 1) }))
+             _ -> outputStrLn ("Don't know how to update repository's " ++ what ++ ".")
+runCommand ["unset", "user", name, what]
+  = do user <- query (GetUser name)
+       case user of
+         Nothing -> outputStrLn "User not found."
+         Just u ->
+           case what of
+             "fullname" -> update (UpdateUser (u { uFullName = "" }))
+             "website" -> update (UpdateUser (u { uWebsite = "" }))
+             "email" -> update (UpdateUser (u { uEmail = "" }))
+             _ -> outputStrLn ("Can't unset user's " ++ what ++ ".")
+runCommand ["unset", "repo", user, name, what]
+  = do repo <- query (GetRepository (user, name))
+       case repo of
+         Nothing -> outputStrLn "Repository not found."
+         Just r ->
+           case what of
+             "description" -> update (UpdateRepository (r { rDescription = "" }))
+             "website" -> update (UpdateRepository (r { rWebsite = "" }))
+             "fork" -> update (UpdateRepository (r { rForkOf = Nothing }))
+             _ -> outputStrLn ("Can't unset repository's " ++ what ++ ".")
 runCommand _ = runCommand ["help"]
 
 admin :: InputT IO ()
