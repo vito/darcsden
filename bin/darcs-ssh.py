6862c1de4765743f28d91d0dda328c51cc4175b0
Alex Suraci <i.am@toogeneric.com>
2010-05-20 21:36:50 +0000

    Updated the SSH server a bit.

--- bin/darcs-ssh.py
+++ bin/darcs-ssh.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+# a small ssh server providing darcsden-specific darcs push/get.
+# and that's all.
+# 
+# packages needed: twisted, pycrypto, pyasn1
+#
+# Once you have CouchDB running, you'll need a "root" user with
+# private_key and public_key values. These will be used for the server.
 
 import base64
 import couchdb
@@ -17,6 +24,7 @@ from zope.interface import implements
 log.startLogging(sys.stderr)
 
 
+BASE = "/srv/darcs"
 COUCH = couchdb.Server("http://localhost:4895/")
 USERS = COUCH['users']
 
@@ -28,6 +36,7 @@ def sane(string):
     Paranoid command security check.
     """
     return (string[0] != "/" and all(map(lambda x: x.isalnum() or x == '-' or x == '_' or x == '/', string)))
+
 
 class DarcsUser(avatar.ConchUser):
     def __init__(self, username):
@@ -58,7 +67,7 @@ class CouchDBChecker(SSHPublicKeyDatabase):
         try:
             keys = USERS[credentials.username]['keys']
 
-            for key in keys.split("\n"):
+            for key in keys:
                 l2 = key.split()
                 if len(l2) < 2:
                     continue
@@ -86,23 +95,21 @@ class DarcsSession:
         elif type == "apply":
             run = ("darcs", "apply", "--all", "--repodir", info[0])
         else:
+            print "got something funny from %s: %s" % (self.user.name, cmd)
             self.reject(proto)
             return
 
         print "safe command: %s" % list(run)
         self.cmd = reactor.spawnProcess(proto, run[0], run)
 
-    def reject(self, proto):
+    def reject(self, proto, msg = "GTFO"):
         proto.write("GTFO.\r\n")
         dp = DenyProtocol()
         dp.makeConnection(proto)
         proto.makeConnection(session.wrapProtocol(dp))
 
-    def openShell(self, trans):
-        trans.write("No shell for you.\r\n")
-        dp = DenyProtocol()
-        dp.makeConnection(trans)
-        trans.makeConnection(session.wrapProtocol(dp))
+    def openShell(self, proto):
+        self.reject(proto, "No shell for you.\r\n")
 
     def eofReceived(self):
         if self.cmd:
@@ -112,16 +119,23 @@ class DarcsSession:
         if command[0] != "darcs":
             return ("unknown", [])
 
-        if command[1] == "transfer-mode" and command[2] == "--repodir" and sane(command[3]) and len(command) == 4:
+        if  command[1] == "transfer-mode" \
+        and command[2] == "--repodir" \
+        and sane(command[3]) \
+        and len(command) == 4:
             return ("transfer-mode", (self.absolute(command[3]),))
 
-        if command[1] == "apply" and command[2] == "--all" and command[3] == "--repodir" and sane(command[4]) and len(command) == 5:
+        if  command[1] == "apply" \
+        and command[2] == "--all" \
+        and command[3] == "--repodir" \
+        and sane(command[4]) \
+        and len(command) == 5:
             return ("apply", (self.absolute(command[4]),))
 
         return ("unknown", [])
 
     def absolute(self, path):
-        return "/srv/darcs/" + self.user.name + "/" + path
+        return BASE + "/" + self.user.name + "/" + path
 
     def getPty(self, term, windowSize, attrs):
         pass
@@ -150,5 +164,5 @@ portal.registerChecker(CouchDBChecker())
 DarcsFactory.portal = portal
 
 if __name__ == '__main__':
-    reactor.listenTCP(5022, DarcsFactory())
+    reactor.listenTCP(22, DarcsFactory())
     reactor.run()
