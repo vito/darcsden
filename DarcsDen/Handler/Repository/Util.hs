5b1992088fa0fa7e147defdb4df41cd11325ceaf
Alex Suraci <i.am@toogeneric.com>
2010-12-19 05:12:54 +0000

    corrected fallback highlighting

--- DarcsDen/Handler/Repository/Util.hs
+++ DarcsDen/Handler/Repository/Util.hs
@@ -5,7 +5,6 @@ import System.Exit
 import System.FilePath (takeExtension)
 import System.IO
 import System.Process
-import Text.XHtml.Strict (renderHtmlFragment)
 import qualified Darcs.Patch as P
 import qualified Darcs.Repository as R
 
@@ -14,24 +13,27 @@ getRepo :: String -> IO (Either String (R.Repository P.Patch))
 getRepo = R.maybeIdentifyRepository []
 
 highlight :: Bool -> String -> String -> IO String
-highlight lineNums fn s = do
-    (pin, pout, _, ph) <- runInteractiveCommand ("pygmentize " ++ unwords args)
-    hPutStr pin s
-    hClose pin
-    res <- waitForProcess ph
-    case res of
-        ExitSuccess ->
-            hGetContents pout
-        ExitFailure _ ->
-            return ("<div class=\"highlight\"><pre>" ++ renderHtmlFragment s ++ "</pre></div>")
+highlight lineNums fn s =
+    highlightAs (filter isAlphaNum (takeExtension fn)) $
+        highlightAs "text" (return "Highlighting failed!")
   where
-    args =
-        [ "-l " ++ filter isAlphaNum (takeExtension fn)
+    args l =
+        [ "-l " ++ l
         , "-f html"
         , if lineNums
              then "-O linenos,lineanchors=L,anchorlinenos"
              else ""
         ]
+
+    highlightAs :: String -> IO String -> IO String
+    highlightAs lexer err = do
+        (pin, pout, _, ph) <- runInteractiveCommand ("pygmentize " ++ unwords (args lexer))
+        hPutStr pin s
+        hClose pin
+        res <- waitForProcess ph
+        case res of
+            ExitSuccess -> hGetContents pout
+            ExitFailure _ -> err
 
 highlightBlob :: String -> String -> IO String
 highlightBlob = highlight True
